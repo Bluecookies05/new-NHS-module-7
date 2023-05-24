258d1f04f2298910fc1e2403dc4d56f1756730a4
Bluecookies05 <bluecookies490@gmail.com>
2023-05-24 14:12:22 -0400

    more python

--- python/firstpython.py
+++ python/firstpython.py
@@ -1,2 +1,22 @@
 #operators
 
+firstNumber = 18
+secondNumber = 20
+
+print ( firstNumber, secondNumber, 78, "Another String", sep="--", end="THE END" )
+print ( "Line 2", end = "Line 2 end" )
+print ( "Another Line" )
+
+#Logical applicator
+
+REQUIRED_MONEY = 20
+REQUIRED_AGE = 18
+
+userAge = 17
+userMoney = 100
+
+if userAge < REQUIRED_AGE or userMoney <REQUIRED_MONEY:
+    print ( "you have to meet the requirements" )
+else:
+    print ( "Enjoy the Party" )
+
